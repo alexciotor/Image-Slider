57977c96f9be802ec957a1b7e7e42226f8f026f6
alexciotor <ciotor.alex@yahoo.com>
2020-09-22 11:32:16 +0300

    Image Slider

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
+
+ //Photo list
 const photos =['./Photos/london1.jpeg','./Photos/london2.jpeg','./Photos/london3.jpg','./Photos/london4.jpeg','./Photos/london5.jpeg'];
-let slider = document.getElementById('slide');
 let i = 0;
-const right =document.getElementById('btn');
-right.addEventListener('click', changePhoto);
 
-const left = document.getElementById('btn2');
-
-left.addEventListener('click', changeLeft);
-
-function changeLeft(){
-slider.style.width="90%";
-slider.src=photos[i];
-if(i>0){
-    i--
-}
-else{
-    i= photos.length-1
-}
-   }   
-
-        
+// Elements
+const slider = document.getElementById('slide');
+let buttons = document.querySelectorAll('.btn'); 
 
 
-
- 
- function changePhoto(){
-slider.style.width="90%";
-slider.src=photos[i]
- if(i < photos.length-1){
-  i++
- }
- else{
-     i = 0;
- }   
-     }
+//Page functionality
+    buttons.forEach(button=>{
+        button.addEventListener('click', function(e){
+            if(button.classList.contains('btn-right')){
+                i++
+                if(i > photos.length - 1){
+                    i = 0
+                }
+                slider.src =photos[i]
+            }
+            if(button.classList.contains('btn-left')){
+                i--
+                if(i < 0){
+                    i = photos.length -1;
+                }
+                slider.src = photos[i];
+            }
+        })
+    })
  
  
